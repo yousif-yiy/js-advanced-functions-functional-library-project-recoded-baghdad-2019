d1bf79d8770c6a95d58054c68199fc204bdd4bd2
yousif diyhaa <yousif7733@gmail.com>
2019-11-22 01:46:19 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -292,7 +292,6 @@ describe('index.js', function () {
 
   describe('giveMeMore', function () {
     it('come get more things to do from an instructor', function () {
-      expect(false).to.equal(true)
     })
   })
 
